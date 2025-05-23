06dd89442a54c34b7d7cee4c500def804dd24d87
RafaelMartins <rafaelsantosmartins.dev@gmail.com>
2025-05-23 14:15:27 +0100

    fixing models

--- CraftingSim/Model/Recipe.cs
+++ CraftingSim/Model/Recipe.cs
@@ -1,32 +1,32 @@
-using System;
 using System.Collections.Generic;
 
 namespace CraftingSim.Model
 {
     public class Recipe : IRecipe
     {
-        public string Name { get; }
+
+        public Recipe(string name, double successRate) 
+        {
+            this.Name = name;
+    this.SuccessRate = successRate;
+   
+        }
+                public string Name { get; }
         public IReadOnlyDictionary<IMaterial, int> RequiredMaterials { get; }
         public double SuccessRate { get; }
-
-        public Recipe(string name, Dictionary<IMaterial, int> materials, double successRate)
+        
+        public Recipe(string name, double successRate, Dictionary<IMaterial, int> materials)
         {
-            Name = name ?? throw new ArgumentNullException(nameof(name));
-            RequiredMaterials = new Dictionary<IMaterial, int>(materials ?? 
-                                                               throw new ArgumentNullException(nameof(materials)));
+            Name = name;
             SuccessRate = successRate;
-
-            if (successRate < 0 || successRate > 1)
-            {
-                throw new ArgumentException("Success rate must be between 0 and 1", 
-                    nameof(successRate));
-            }
+            RequiredMaterials = materials;
         }
-
-        public int CompareTo(IRecipe other)
+        
+        public int CompareTo(IRecipe obj)
         {
-            if (other == null) return 1;
-            return string.Compare(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+            if (obj == null) return 1;
+            return Name.CompareTo(obj.Name);
         }
+        
     }
 }
